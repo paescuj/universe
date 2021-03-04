8262445b72f15776879dc1c97cb5421ea5a9eef2
Pascal Jufer <pascal-jufer@bluewin.ch>
2021-03-04 12:00:30 +0100

    Use cache, create unsorted file, cleanup

--- go.mod
+++ go.mod
@@ -1,5 +1,10 @@
 module github.com/paescuj/universe
 
-go 1.15
+go 1.16
 
-require github.com/google/go-github/v33 v33.0.0
+require (
+	github.com/google/btree v1.0.0 // indirect
+	github.com/google/go-github/v33 v33.0.0
+	github.com/gregjones/httpcache v0.0.0-20190611155906-901d90724c79
+	github.com/peterbourgon/diskv v2.0.1+incompatible
+)
--- main.go
+++ main.go
@@ -3,14 +3,36 @@ package main
 import (
   "os"
   "fmt"
+  "sort"
   "context"
   "text/template"
   "io/ioutil"
   "encoding/json"
+  "net/http"
   "github.com/google/go-github/v33/github"
+  "github.com/gregjones/httpcache"
+  "github.com/gregjones/httpcache/diskcache"
 )
 
-func fetchUniverseTest() ([]*github.Repository, error) {
+type SortedUniverse struct {
+  LivingStars []*github.Repository
+  DeadStars   []*github.Repository
+}
+
+func (s SortedUniverse) Count() int {
+  return len(s.LivingStars)+len(s.DeadStars)
+}
+
+func SaveTestData(universe []*github.Repository) (error) {
+  file, _ := json.MarshalIndent(universe, "", " ")
+  err := ioutil.WriteFile("test.json", file, 0644)
+  if err != nil {
+    return err
+  }
+  return nil
+}
+
+func TestFetchUniverse() ([]*github.Repository, error) {
   var universe []*github.Repository
 	file, err := ioutil.ReadFile("test.json")
   if err != nil {
@@ -23,50 +45,76 @@ func fetchUniverseTest() ([]*github.Repository, error) {
   return universe, nil
 }
 
-func fetchUniverse(username string) ([]*github.Repository, error) {
-  ctx := context.Background()
-  client := github.NewClient(nil)
-  opt := &github.ActivityListStarredOptions{
-    Sort: "updated",
+func FetchUniverse(username string) ([]*github.Repository, error) {
+  context := context.Background()
+  cachedir := ".cache"
+	cache := diskcache.New(cachedir)
+	transport := httpcache.NewTransport(cache)
+	httpClient := &http.Client{Transport: transport}
+  githubClient := github.NewClient(httpClient)
+  githubOpts := &github.ActivityListStarredOptions{
     ListOptions: github.ListOptions{PerPage: 100},
   }
 
   var universe []*github.Repository
   for {
-    stars, resp, err := client.Activity.ListStarred(ctx, username, opt)
+    stars, response, err := githubClient.Activity.ListStarred(context, username, githubOpts)
     if err != nil {
       return nil, err
     }
 	  for _, star := range stars {
       universe = append(universe, star.GetRepository())
 	  }
-    if resp.NextPage == 0 {
+    if response.NextPage == 0 {
       break
     }
-    opt.Page = resp.NextPage
+    githubOpts.Page = response.NextPage
   }
   return universe, nil
 }
 
 func main() {
-  tpl, err := template.ParseGlob("templates/*.gomd")
+  // Load templates
+  template, err := template.ParseGlob("templates/*.gomd")
   if err != nil {
 		fmt.Printf("Error: %v\n", err)
 		return
 	}
 
-  universe, err := fetchUniverse("paescuj")
-  //universe, err := fetchUniverseTest()
+  // Fetch universe
+  //universe, err := FetchUniverse("paescuj")
+  universe, err := TestFetchUniverse()
 	if err != nil {
 		fmt.Printf("Error: %v\n", err)
 		return
 	}
 
-  file, err := os.Create("README.md")
+  // Init UNSORTED.md file
+  unsortedFile, err := os.Create("UNSORTED.md")
+  if err != nil {
+    fmt.Printf("Error: %v\n", err)
+    return
+  }
+
+  // Write UNSORTED.md file
+  err = template.ExecuteTemplate(unsortedFile, "UNSORTED.gomd", universe)
+  unsortedFile.Close()
+  if err != nil {
+    fmt.Printf("Error: %v\n", err)
+    return
+  }
+
+  // Init README.md file
+  readmeFile, err := os.Create("README.md")
   if err != nil {
 		fmt.Printf("Error: %v\n", err)
     return
   }
+
+  // Sort universe
+  sort.Slice(universe, func(i, j int) bool {
+    return universe[i].GetPushedAt().Time.After(universe[j].GetPushedAt().Time)
+  })
 
   sortedUniverse := SortedUniverse{}
   for _, star := range universe {
@@ -77,8 +125,9 @@ func main() {
     }
   }
 
-  err = tpl.Execute(file, sortedUniverse)
-  file.Close()
+  // Write README.md file
+  err = template.ExecuteTemplate(readmeFile, "README.gomd", sortedUniverse)
+  readmeFile.Close()
 	if err != nil {
 		fmt.Printf("Error: %v\n", err)
 		return
